661940ff84c9bf3e97be4edad63b67ccbcfb28e7
nniabb <niabilanishvili17@gmail.com>
2023-12-11 22:49:30 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// დავალება 1
+//const input = [1, 2, 3, [4, 5, [6, 7, [8, 9]]]];
+//const result = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function flattenArray(input) {
+    return [].concat.apply([], input.map(item => Array.isArray(item) ? flattenArray(item) : item))
+}
+
+const input = [1, 2, 3, [4, 5, [6, 7, [8, 9]]]]
+const output = flattenArray(input)
+console.log(output)
+
+
+//const input1 = [1, 2, 3, [4, 5, [6, 7, [8, 9]]], 10, 11, 12];
+//const result1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+function random(input) {
+    let res = []
+    function rand(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                rand(arr[i])
+            } else {
+                res.push(arr[i])
+            }
+        }
+    }
+    rand(input)
+    return res
+}
+
+const input1 = [1, 2, 3, [4, 5, [6, 7, [8, 9]]], 10, 11, 12]
+const result = random(input1)
+console.log(result)
+
+
+
+
+  
